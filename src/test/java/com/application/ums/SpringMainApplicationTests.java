22f9d7f8dd7b3217ad135080b70e6b67a03af0e8
Sathish-Kumar-E <sathishkumar13121998@gmail.com>
2024-05-05 19:55:22 -0400

    Added maven dependencies for Jasper and JSTL

---
+++ src/test/java/com/application/ums/SpringMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.ums;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
